994b6b5e86bd4ef21e90ee6d0538d8ab71d03641
neesha321 <taraheroine@gmail.com>
2018-08-01 14:13:55 +0545

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginpageComponent} from './loginpage/loginpage.component';
+import {WelcomepageComponent} from './welcomepage/welcomepage.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  { path: 'login', component:LoginpageComponent }
+];
+
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
